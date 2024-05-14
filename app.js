b10b6c4fa0d336f2f2302437a28653e508aa91a6
Myaseer34 <123508992+Myaseer34@users.noreply.github.com>
2024-05-14 21:00:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const textElement = document.getElementById('text');
+const colors = ['#de164f', '#afa8a8', '#4fc1e9', '#a2d050']; // Array of colors
+
+let currentColorIndex = 0;
+
+function changeColor() {
+  textElement.style.color = colors[currentColorIndex];
+  currentColorIndex = (currentColorIndex + 1) % colors.length; // Loop through the colors array
+}
+
+// Seconds variable
+const seconds = .5
+setInterval(changeColor, seconds * 1000); // Call changeColor function every 500ms
